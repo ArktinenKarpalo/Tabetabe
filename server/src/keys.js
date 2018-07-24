c2bcd11cbf2046ede8fd40ffba9bbc8df1749b65
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2018-07-24 16:42:37 +0300

    Add keys.js template

---
+++ server/src/keys.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+module.exports = {
+  // OAuth2 Client Credentials
+	google: {
+		clientID: "googleID",
+		clientSecret: "googleSecret"
+	},
+  // MongoDB connection URI
+	mongodb: {
+		dbURI: "mongodb://"
+	}
+};
